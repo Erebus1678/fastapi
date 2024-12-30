62e5c47a497ff31f09dffbfdaf968c07f0e62871
Erebus1678 <dmitryi.platov@gmail.com>
2024-12-30 12:49:56 +0200

    Add FastAPI endpoints for posts and implement Post model

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Welcome to the Panic room!"}
+
+@app.get("/posts")
+def get_posts():
+    return{'data':'Here gonna be a posts'}
+
+@app.post('/createposts')
+def create_post(post:Post):
+    return {'msg':'new post was created successfully'}
