53333035d4c3dbd2f92770bdb68c1cb00ad797eb
Erebus1678 <dmitryi.platov@gmail.com>
2024-12-30 14:36:17 +0200

    Implement CRUD operations for posts with FastAPI

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
@@ -9,6 +10,14 @@ class Post(BaseModel):
     content: str
     published: bool = True
     rating: Optional[int] = None
+    
+    
+my_posts = [{"id":1,"title": "Post 1", "content" : "Example content for post 1",}, {"id":2, "title": "Post 2", "content":"Example content for post 2"}]
+
+def find_post(id):
+    for p in my_posts:
+        if p['id'] == id:
+            return p
 
 @app.get("/")
 async def root():
@@ -16,8 +25,17 @@ async def root():
 
 @app.get("/posts")
 def get_posts():
-    return{'data':'Here gonna be a posts'}
+    return{"data": my_posts}
 
-@app.post('/createposts')
+@app.post("/posts")
 def create_post(post:Post):
-    return {'msg':'new post was created successfully'}
+    post_dict = post.model_dump()
+    post_dict['id'] = randrange(0, 10000000)
+
+    my_posts.append(post_dict)
+    return{"data": post_dict}
+
+@app.get("/posts/{id}")
+def get_post(id:int):
+    post = find_post(id)
+    return {"post_details": post}
